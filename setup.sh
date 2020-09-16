e93686c5122b5f9e0880165fc9bb1942c1c3678f
AbhiSivaprasad <abhi@live.com>
2020-09-16 15:13:04 -0700

    source bashrc at the end of setup

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@ ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/tmux ~/.tmux
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 
-# source custom bashrc from default bashrc
-echo "source ~/.dotfiles/bashrc.cust" >> ~/.bashrc
-
 # install all submodules
 cd ~/.dotfiles
 git submodule update --init --recursive
+
+# source custom bashrc from default bashrc
+echo "source ~/.dotfiles/bashrc.cust" >> ~/.bashrc
+source ~/.bashrc
